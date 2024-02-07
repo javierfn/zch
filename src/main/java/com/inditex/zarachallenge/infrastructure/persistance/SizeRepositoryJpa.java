6b4b824fde77e27f4311dfebbeb3006700107249
Javier Fernández Nogueira <jfernandezn@externos.itnow.es>
2024-02-07 21:50:06 +0100

    ZCH-0001: Corrections

--- src/main/java/com/inditex/zarachallenge/infrastructure/persistance/SizeRepositoryJpa.java
+++ src/main/java/com/inditex/zarachallenge/infrastructure/persistance/SizeRepositoryJpa.java
@@ -2,7 +2,8 @@ package com.inditex.zarachallenge.infrastructure.persistance;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.inditex.zarachallenge.infrastructure.persistance.entity.OfferEntity;
+import com.inditex.zarachallenge.infrastructure.persistance.entity.SizeEntity;
 
-public interface SizeRepositoryJpa extends JpaRepository<SizeRepositoryJpa, Long> {
+public interface SizeRepositoryJpa extends JpaRepository<SizeEntity, Long> {
 
 }
