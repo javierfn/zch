00e7fa154be7ba6d02fafa5516402826770e3943
Javier Fernández Nogueira <jfernandezn@externos.itnow.es>
2024-02-09 14:14:39 +0100

    ZCH-0001: Added test for SimilarController

--- src/main/java/com/inditex/zarachallenge/controller/SimilarController.java
+++ src/main/java/com/inditex/zarachallenge/controller/SimilarController.java
@@ -1,6 +1,31 @@
 package com.inditex.zarachallenge.controller;
 
-public class SimilarController {
+import java.util.HashSet;
+import java.util.Set;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import com.inditex.zarachallenge.application.SimilarProductService;
+import com.inditex.zarachallenge.infrastructure.dto.ProductDetailDTO;
+import com.inditex.zarachallenge.infrastructure.mappers.ProductDetailMapper;
+import lombok.AllArgsConstructor;
 
-	// TODO: Insert code in this controller
+@RestController
+@AllArgsConstructor
+public class SimilarController implements SimilarControllerInterface {
+
+  private final SimilarProductService similarProductService;
+
+  private final ProductDetailMapper productDetailMapper;
+
+  @Override
+  public ResponseEntity<Set<ProductDetailDTO>> productProductIdSimilarGet(String productId) {
+     return ResponseEntity.ok(
+         new HashSet<>(
+             productDetailMapper.toDto(
+                 similarProductService.findSimilarProductsByProductId(Long.valueOf(productId))
+             )
+         )
+     );
+  }
+
 }
--- src/test/java/com/inditex/zarachallenge/controller/SimilarControllerTest.java
+++ src/test/java/com/inditex/zarachallenge/controller/SimilarControllerTest.java
@@ -1,56 +1,128 @@
 package com.inditex.zarachallenge.controller;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import org.jeasy.random.EasyRandom;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockserver.model.HttpStatusCode;
+import com.inditex.zarachallenge.application.ProductServiceImpl;
+import com.inditex.zarachallenge.application.SimilarProductService;
+import com.inditex.zarachallenge.domain.exception.OfferNotFoundException;
+import com.inditex.zarachallenge.domain.exception.ProductNotFoundException;
+import com.inditex.zarachallenge.domain.exception.SizeNotFoundException;
+import com.inditex.zarachallenge.domain.model.Offer;
+import com.inditex.zarachallenge.domain.model.Product;
+import com.inditex.zarachallenge.domain.model.ProductDetail;
+import com.inditex.zarachallenge.domain.model.Size;
+import com.inditex.zarachallenge.domain.repository.OfferRepository;
+import com.inditex.zarachallenge.domain.repository.ProductRepository;
+import com.inditex.zarachallenge.domain.repository.SizeRepository;
+import com.inditex.zarachallenge.infrastructure.mappers.OfferMapper;
+import com.inditex.zarachallenge.infrastructure.mappers.ProductDetailMapper;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 
-@SpringBootTest
-@WebAppConfiguration
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@Epic("Product management")
 class SimilarControllerTest {
-	@Autowired
-	private WebApplicationContext webApplicationContext;
-	private MockMvc mockMvc;
-	private static String URL = "/product/{id}/similar";
-	
-	@BeforeEach
-	public void setup() throws Exception {
-	    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
-	}
 
-	@Test
-	void testInput9() throws Exception {
-		mockMvc.perform(get(URL, 9)).andExpect(status().is(HttpStatus.OK.value()))
-				.andExpect(jsonPath("$.[0].id", is("11"))).andExpect(jsonPath("$.[0].name", is("Cotton T-shirt")))
-				.andExpect(jsonPath("$.[0].price", is(39.99))).andExpect(jsonPath("$.[0].availability", is(false)))
-				.andExpect(jsonPath("$.[1].id", is("15"))).andExpect(jsonPath("$.[1].name", is("Button-up shirt")))
-				.andExpect(jsonPath("$.[1].price", is(49.99))).andExpect(jsonPath("$.[1].availability", is(true)))
-				.andExpect(jsonPath("$.[2].id", is("19"))).andExpect(jsonPath("$.[2].name", is("Linen pants")))
-				.andExpect(jsonPath("$.[2].price", is(29.99))).andExpect(jsonPath("$.[2].availability", is(true)))
-				.andReturn().getResponse().getContentAsString();
-	}
+    EasyRandom generator;
 
-	@Test
-	void testInput12() throws Exception {
-		System.out.println(mockMvc.perform(get(URL, 12)).andReturn().getResponse().getContentAsString());
-		mockMvc.perform(get(URL, 12)).andExpect(status().is(HttpStatus.OK.value()))
-				.andExpect(jsonPath("$.[0].id", is("20"))).andExpect(jsonPath("$.[0].name", is("Polo shirt")))
-				.andExpect(jsonPath("$.[0].price", is(24.99))).andExpect(jsonPath("$.[0].availability", is(false)))
-				.andExpect(jsonPath("$.[1].id", is("18"))).andExpect(jsonPath("$.[1].name", is("Chinos")))
-				.andExpect(jsonPath("$.[1].price", is(59.99))).andExpect(jsonPath("$.[1].availability", is(true)))
-				.andExpect(jsonPath("$.[2].id", is("19"))).andExpect(jsonPath("$.[2].name", is("Linen pants")))
-				.andExpect(jsonPath("$.[2].price", is(29.99))).andExpect(jsonPath("$.[2].availability", is(true)))
-				.andReturn().getResponse().getContentAsString();
-	}
+    ProductDetailMapper productDetailMapper;
+
+    @Mock
+    SimilarProductService similarProductService;
+
+    SimilarController similarController;
+
+    @BeforeEach
+    void setup() {
+        generator = new EasyRandom();
+
+        productDetailMapper = Mappers.getMapper(ProductDetailMapper.class);
+
+        similarController = new SimilarController(similarProductService, productDetailMapper);
+    }
+
+    @Test
+    @Feature("Find similar products")
+    @Story("Shall return error")
+    void should_throw_number_format_exception_when_productProductIdSimilarGet_and_product_id_is_not_valid_string_number() {
+
+        var productId = "productIdError";
+
+        assertThrows(NumberFormatException.class,
+            () -> similarController.productProductIdSimilarGet(productId),
+        "Should throw number format exception");
+    }
+
+    @Test
+    @Feature("Find similar products")
+    @Story("Shall return product detail data")
+    void should_return_http_ok_with_empty_list_when_productProductIdSimilarGet_and_there_are_not_similar_products() {
+
+        var productId = "1";
+
+        var productDetailList = new ArrayList<ProductDetail>();
+
+        when(similarProductService.findSimilarProductsByProductId(anyLong()))
+            .thenReturn(productDetailList);
+
+        var result = similarController.productProductIdSimilarGet(productId);
+
+        assertNotNull(result, "Result should be not null");
+        assertEquals(HttpStatusCode.OK_200.code(), result.getStatusCode().value(), "Http status code should be 200 OK");
+        assertNotNull(result.getBody(), "Body should be not null");
+        assertTrue(result.getBody().isEmpty(), "Result set should be empty");
+    }
+
+    @Test
+    @Feature("Find similar products")
+    @Story("Shall return product detail data")
+    void should_return_http_ok_with_a_list_when_productProductIdSimilarGet_and_there_are_similar_products() {
+
+        var productId = "1";
+
+        var productDetailList = generator.objects(ProductDetail.class,5)
+            .collect(Collectors.toList());
+
+        when(similarProductService.findSimilarProductsByProductId(anyLong()))
+            .thenReturn(productDetailList);
+
+        var result = similarController.productProductIdSimilarGet(productId);
+
+        assertNotNull(result, "Result should be not null");
+        assertEquals(HttpStatusCode.OK_200.code(),
+            result.getStatusCode().value(),
+            "Http status code should be 200 OK");
+        assertNotNull(result.getBody(), "Body should be not null");
+        assertFalse(result.getBody().isEmpty(), "Result set should be not empty");
+        assertEquals(productDetailList.size(),
+            result.getBody().size(),
+            "Result set size should be equals");
+    }
 
 }
